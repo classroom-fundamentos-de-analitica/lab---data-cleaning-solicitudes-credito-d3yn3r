23c19f126d6df6cb7d23ff1e21c1317e1f8dde19
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2023-05-01 02:41:46 -0500

    cambio de fecha

--- pregunta.py
+++ pregunta.py
@@ -7,7 +7,8 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+import re
+from datetime import datetime
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
@@ -17,7 +18,7 @@ def clean_data():
     df.monto_del_credito = df.monto_del_credito.str.replace(',','') 
     df.monto_del_credito = df.monto_del_credito.astype(float)
     df.monto_del_credito = df.monto_del_credito.astype(int)
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)  
+    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))  
     df.comuna_ciudadano = df.comuna_ciudadano.apply(float)
     #colString = df.select_dtypes(include=['object']).columns.tolist()
     
