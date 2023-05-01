fe59d06cd82bec9d4766ceeb529ea9cfaaf612f1
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2023-05-01 02:32:45 -0500

    corrección fecha

--- pregunta.py
+++ pregunta.py
@@ -11,22 +11,20 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
-    
-    df.monto_del_credito = df.monto_del_credito.str.replace(',','') 
+
+    df.dropna(axis=0, inplace=True)
     df.monto_del_credito = df.monto_del_credito.str.strip('$')
+    df.monto_del_credito = df.monto_del_credito.str.replace(',','') 
     df.monto_del_credito = df.monto_del_credito.astype(float)
     df.monto_del_credito = df.monto_del_credito.astype(int)
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
-    df.dropna(axis=0, inplace=True)
-
-      
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)  
 
     colString = df.select_dtypes(include=['object']).columns.tolist()
     
     for i in colString:
         df[i] = df[i].str.lower()
-        df[i] = df[i].str.replace('-',' ')
         df[i] = df[i].str.replace('_',' ')
+        df[i] = df[i].str.replace('-',' ')
     
     df.drop_duplicates(inplace=True)
 
