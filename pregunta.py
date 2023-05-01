fac8a10715833ebc5e290042ede230c0e8b49f7f
d3yn3r <50970396+d3yn3r@users.noreply.github.com>
2023-05-01 02:12:08 -0500

    solucion

--- pregunta.py
+++ pregunta.py
@@ -8,13 +8,26 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 """
 import pandas as pd
 
-
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    
+    df.monto_del_credito = df.monto_del_credito.str.replace(',','') 
+    df.monto_del_credito = df.monto_del_credito.str.strip('$')
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
 
-    #
-    # Inserte su código aquí
-    #
+    df.dropna(axis=0, inplace=True)
+
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)  
+
+    colString = df.select_dtypes(include=['object']).columns.tolist()
+    
+    for i in colString:
+        df[i] = df[i].str.lower()
+        df[i] = df[i].replace('-',' ')
+        df[i] = df[i].replace('_',' ')
+    
+    df.drop_duplicates(inplace=True)
 
     return df
